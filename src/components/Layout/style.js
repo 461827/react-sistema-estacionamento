05eaa36a318c314b032908684d4e4009e5e66fa3
VitorGabriel <vg285757@gmail.com>
2021-09-03 19:29:45 -0300

    ajustando style layout

--- src/components/Layout/style.js
+++ src/components/Layout/style.js
@@ -4,10 +4,7 @@ export const Container = Styled.div`
     width: 320px;
     height: 580px;
     margin: 30px auto 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+   
     font-size: 18px;
     border: 2px solid #dedede;
     border-radius: 20px;
