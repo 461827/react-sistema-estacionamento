aec39aaa085d037a6535652bc24794452b0ab8ff
VitorGabriel <vg285757@gmail.com>
2021-09-03 20:16:55 -0300

    Construindo Primeria Tela

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Input } from "./components/Input";
 import { Title } from "./components/Title";
 import { Subtitle } from "./components/Subtitle";
 import { Layout } from "./components/Layout";
+import {Space} from "./components/Space";
 
 
 function App() {
@@ -11,8 +12,39 @@ function App() {
     <div>
       <Layout>
         <Title />
-        <Subtitle />
-        <Input />
+
+        <Space top={6}/>
+
+        <Subtitle>Adicionar</Subtitle>
+
+        <Space top={4}/>
+
+        <Input 
+          label="Nome do Proprietario" 
+          placeholder="Digite o nome" 
+          top={2}
+        />
+
+        <Input 
+          label="Placa do Veiculo" 
+          placeholder="Digite a placa" 
+          top={2}
+        />
+
+        <Input 
+          label="Nome do Funcionario" 
+          placeholder="Selecione" 
+          top={2}
+        />
+
+        <Input 
+          label="Vaga" 
+          placeholder="Selecione" 
+          top={2}
+        />
+
+        <Space top={4}/>
+
         <Button label="Adicionar 1"/>
       </Layout>
     </div>
