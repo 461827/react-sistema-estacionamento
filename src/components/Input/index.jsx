8baa498ee0bafe65fe3f82ee6384e97638761ee2
VitorGabriel <vg285757@gmail.com>
2021-09-03 20:15:44 -0300

    estilizando o component input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import {Container,Label, InputText} from "./style"
 
-export const Input = ({label,placeholder}) => {
+import {Space} from "../Space"
+
+export const Input = ({label,placeholder,top}) => {
     return(
-        <Container>
-            <Label htmlFor="">{label}</Label>
-            <InputText type="text" placeholder={placeholder} />
-        </Container>
+        <>
+            <Container>
+                <Label htmlFor="">{label}</Label>
+                <InputText type="text" placeholder={placeholder} />
+            </Container>
+            <Space top={top? top: 0}/>
+        </>
     )
 } 
 
--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -7,6 +7,7 @@ export const Container = Styled.div`
 export const InputText = Styled.input`
     width: 100%;
     padding:8px 20px;
+    margin-top: 4px;
     box-sizing: border-box;
     border: 0px;
     border-bottom: 1px solid #1F2D3D;
