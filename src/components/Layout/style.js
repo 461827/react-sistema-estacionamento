6b06c3fea4c7984080277813b046154f0eb3af3d
VitorGabriel <vg285757@gmail.com>
2021-09-03 20:16:25 -0300

    Evoluindo o Layout

--- src/components/Layout/style.js
+++ src/components/Layout/style.js
@@ -4,7 +4,9 @@ export const Container = Styled.div`
     width: 320px;
     height: 580px;
     margin: 30px auto 0;
-   
+    padding:20px;
+    border: 0px;
+    border-bottom: 1px solid #1F2D3D;
     font-size: 18px;
     border: 2px solid #dedede;
     border-radius: 20px;
