4084fdcfd975c375cdc37509d6a737530cc917f9
VitorGabriel <vg285757@gmail.com>
2021-09-08 19:27:30 -0300

    adicionando navegação entre telas

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
-import React from "react";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
-import { Title } from "./components/Title";
-import { Subtitle } from "./components/Subtitle";
-import { Layout } from "./components/Layout";
-import {Space} from "./components/Space";
+import React,{useState} from "react";
 
+import PageForm from "./pages/Form";
+import PageList from "./pages/List";
 
 function App() {
+  const [route,setRoute] = useState("register");
+
   return (
-    <div>
-      <Layout>
-        <Title />
-
-        <Space top={6}/>
-
-        <Subtitle>Adicionar</Subtitle>
-
-        <Space top={4}/>
-
-        <Input 
-          label="Nome do Proprietario" 
-          placeholder="Digite o nome" 
-          top={2}
-        />
-
-        <Input 
-          label="Placa do Veiculo" 
-          placeholder="Digite a placa" 
-          top={2}
-        />
-
-        <Input 
-          label="Nome do Funcionario" 
-          placeholder="Selecione" 
-          top={2}
-        />
-
-        <Input 
-          label="Vaga" 
-          placeholder="Selecione" 
-          top={2}
-        />
-
-        <Space top={4}/>
-
-        <Button label="Adicionar 1"/>
-      </Layout>
-    </div>
+    <> 
+    {route === "home" && <PageList setRoute={setRoute}/>}
+    {route === "register" && <PageForm setRoute={setRoute}/>}
+      
+    </>
   );
 }
 
