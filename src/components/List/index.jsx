3f629dd47da45231bd72b3d15bfd22880e5e0a34
VitorGabriel <vg285757@gmail.com>
2021-09-08 19:29:49 -0300

    criando component list

---
+++ src/components/List/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import {Container} from "./style";
+import {Item} from "./Item"
+import {Space} from "../Space"
+
+
+export const List = () => {
+    return(
+       <Container>
+            <Item/> 
+            <Item/>        
+            <Item/>        
+            <Item/>        
+            <Item/>        
+            <Item/>        
+
+       </Container>
+    )
+}
