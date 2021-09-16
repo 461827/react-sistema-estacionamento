6202f2b116261d986fcde23a13cf516b14df5019
VitorGabriel <vg285757@gmail.com>
2021-09-16 16:58:49 -0300

    Disponibilizando funções para uso no sistema

--- src/db/index.js
+++ src/db/index.js
@@ -23,5 +23,46 @@ export const db = [
       parkingSpace: 5
 
     }
-
 ]
+
+// ALTERAR UM ITEM
+export const updateItemV2 = (data) => {
+  let alvo = db.findIndex((item) => {
+    return item.parkingSpace === data.parkingSpace  
+  })
+  
+  if(alvo === -1){
+    return
+  }
+  
+  db.splice(alvo, 1);
+
+  db.push(data)
+ 
+}
+
+const dataIUptaded = {
+  nameClient: "Victor",
+  model: "Civic", 
+  plate: "ABB2341",
+  employee: "André",
+  parkingSpace: 2
+}
+//updateItemV2(dataIUptaded)
+
+// BUSCAR UM ITEM
+export const selecionaVeiculo = (vaga) => {
+  console.log(db.find((veiculo)=>{return veiculo.parkingSpace === vaga}))
+} 
+//selecionaVeiculo(6)
+
+// REMOVER UM ITEM
+export const removeItem = (vaga) => {
+  let alvo = db.findIndex((item) => {
+    return item.parkingSpace === vaga 
+  })
+
+  db.splice(alvo,1);
+
+}
+//removeItem(6);
