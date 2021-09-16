011956b14d7d1a4445c51023fc79a0b6aa4e5599
VitorGabriel <vg285757@gmail.com>
2021-09-16 18:50:14 -0300

    Atualizando a List

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 import {Container} from "./style";
 import {Item} from "./Item"
 
 export const List = ({data}) => {
-    return(
+    const [dataList, setDataList] = useState([]);
+    const [refresh, setRefresh] = useState("")
+    
+    useEffect(() => {
+        setDataList(data)
+    }, [refresh])  
+        
+   return(
        <Container>
-           {data.map( (item,key) => {
-               return <Item data={item} key={key}/> 
+           {dataList.map( (item,key) => {
+               return <Item data={item} key={key} setRefresh={setRefresh}/> 
            })}
        </Container>
     )
